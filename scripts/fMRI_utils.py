9da6c7c297cc9a94ea29c1d7fcfc11b8ff7df8bb
Artyom K <artyomkarpov@gmail.com>
2023-03-17 15:16:12 +0300

    Changes to download data (unfinished)

--- scripts/fMRI_utils.py
+++ scripts/fMRI_utils.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from nilearn.masking import compute_epi_mask, apply_mask
 import nibabel as nib
 import numpy as np
+from subprocess import run
 
 datapath = Path('./data')
 
@@ -18,7 +19,20 @@ datapath = Path('./data')
 def generate_flattened_data():
     #download the dataset into the data folder, but don't add it to git
     fmri_path = datapath / "ds000212"
-    scenarios = np.loadtxt(datapath / "scenarios.csv", delimiter=',', quotechar='"', dtype=str) 
+    scenarios = np.loadtxt(
+        datapath / "scenarios.csv", 
+        delimiter=',',
+        quotechar='"', 
+        dtype= [
+            ('item', 'S20'),
+            ('type','U9999'),
+            ('background', 'U9999'),
+            ('action','U9999'),
+            ('outcome','U9999'),
+            ('accidental','U9999'),
+            ('intentional','U9999'),
+        ]
+    ) 
 
     #this data structure will allow us to capture anatomical or functional data dependent on run and subject
     layout = bids.BIDSLayout(fmri_path, config=['bids', 'derivatives'])
@@ -29,26 +43,44 @@ def generate_flattened_data():
         target_path.mkdir(parents=True, exist_ok=True)
         k = 0
         for bold_f in subject_path.glob("*.nii.gz"):
-            mask_img = compute_epi_mask(bold_f)
-            masked_data = apply_mask(bold_f, mask_img)
-            k = k+1
-            filename = datapath / f"functional_flattened/sub-{i}/{k}.npy"
-            #create file
-            np.save(filename, masked_data)
+            info(f'Processing {bold_f.name}')
+            #   bold_symlink_f = str(bold_f.resolve())
+            #   mask_img = compute_epi_mask(bold_symlink_f)
+            #   masked_data = apply_mask(bold_symlink_f, mask_img)
+            #   k = k+1
+            #   filename = datapath / f"functional_flattened/sub-{i}/{k}.npy"
+            #   #create file
+            #   np.save(filename, masked_data)
 
             event_file = bold_f.parent / bold_f.name.replace('_bold.nii.gz', '_events.tsv')
-            events = np.loadtxt(event_file, delimiter='\t', dtype=str)
+            #events = np.loadtxt(event_file, delimiter='\t', dtype={'names':('onset','duration','condition','item','key','RT'), 'formats':(int,int,str,int,int,float)})
+            events = np.loadtxt(event_file,dtype=str)[1:]
+
             this_scenarios = []
             for e in events:
-                skind, stype = event_to_scenario[e['condition']]
-                item = e['item']
-                found = next(s for s in scenarios if s['item'] == item)
+                condition, item = e[2], e[3]
+                if condition not in event_to_scenario:
+                    info(f'Skipping event {e}: no scanario mapping.')
+                    continue
+                skind, stype = event_to_scenario[condition]
+                found = [s for s in scenarios if s['item'] == item]
+                if not found:
+                    info(f'Skipping event {e}: no scenario with this item found.')
+                    continue
+                found = found[0]
                 assert found['type'] == stype, f"Scenario with {item} item does not match the '{stype}' expected type. Scenario: {found}. Event: {e}."
                 text = f"{found['background']} {found['action']} {found['outcome']} {found[skind]}"
                 this_scenarios.append(text)
             events_f = datapath / f"functional_flattened/sub-{i}/labels-{k}.npy"
             np.save(events_f, np.array(this_scenarios))
 
+def download_dataset():
+    cmd = 'cd data; datalad install --get-data https://github.com/OpenNeuroDatasets/ds000212.git'
+    cprocess = run(cmd, shell=True)
+    assert cprocess.returncode==0, f'"`{cmd}` failed: {cprocess}'
+
+def info(msg):
+    print(msg)
 
 event_to_scenario = {
     "A_PHA": ("accidental", "Physical harm"),
@@ -64,6 +96,7 @@ event_to_scenario = {
 }
 
 def main():
+    download_dataset()
     generate_flattened_data()
 
     #load out of the numpy files as so
