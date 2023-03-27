f34fd8c1e200304fdccb696a3f23933b0b66cb82
RaymondKoopmanschap <raymond95@hotmail.nl>
2023-03-27 11:54:10 +0200

    Added datalad as requirments and added utf-8 encoding to make the fMRI utils also work on Windows

--- bin/fMRI_utils.py
+++ bin/fMRI_utils.py
@@ -134,7 +134,7 @@ def extract_scenarios(subject, run_num, bold_f):
 
 def init_scenarios():
     global all_scenarios
-    with open(datapath / "scenarios.csv", newline='') as csvfile:
+    with open(datapath / "scenarios.csv", newline='', encoding='utf-8') as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             all_scenarios.append(row)
