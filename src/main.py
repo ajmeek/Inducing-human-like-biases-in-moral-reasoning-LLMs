2bf8d078a10790b118f867d334cf40b0a6e0ca01
Artyom K <artyomkarpov@gmail.com>
2023-04-04 19:13:14 +0300

    Fixing bug after invalid merge

--- src/main.py
+++ src/main.py
@@ -84,6 +84,7 @@ def main():
     trainer.fit(lit_model, train_loader)
 
     # Use base model with new head for testing.
+    test_head_dims=[2]
     model = BERT(base_model, head_dims=test_head_dims)
     lit_model = LitBert(model, config['only_train_head'])  # losses are not needed for testing
 
