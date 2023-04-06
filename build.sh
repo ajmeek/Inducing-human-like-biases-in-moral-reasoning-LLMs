09caa0e25f7b82ba1110cc118eb4d9b151b6d3be
Artyom K <artyomkarpov@gmail.com>
2023-04-06 15:28:41 +0300

    Running in Google Colab function

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 set -euo pipefail
 IFS=$'\n\t'
 trap "echo 'error: Script failed: see failed command above'" ERR
@@ -9,15 +9,42 @@ script_dir="$(dirname "$script_path")"
 readonly script_dir
 root_dir=$( realpath "$script_dir/.." )
 
-if [[ ! -e data/ds000212  || ! -e data/functional_flattened ]]; then 
-    echo Downloading and processing ds000212...
-    python bin/fMRI_utils.py
+function load_data() 
+{
+    echo Loading data...
+    if [[ ! -e data/ds000212  || ! -e data/functional_flattened ]]; then 
+        echo Downloading and processing ds000212...
+        python bin/fMRI_utils.py
+    fi
+
+    if [[ ! -e data/ethics ]]; then 
+        echo Downloading and processing ETHICS...
+        bash ./bin/download_ethics_ds.sh 
+    fi
+}
+
+function train()
+{
+    load_data
+    echo Training
+    bash bin/train.sh --num_epochs=0 --only_train_head=True  --num_samples_test=1000 --num_samples_train=3000 --ethics_num_epochs=1
+}
+
+function google_colab()
+{
+    apt install git-annex
+    train
+}
+
+if [[ $# == 0 ]] ; then 
+    echo 'Usage: build.sh <FUNCTION>
+
+FUNCTION: 
+  train     - runs training
+  load_data - loads data
+'
+else 
+    $1
 fi
 
-if [[ ! -e data/ethics ]]; then 
-    echo Downloading and processing ETHICS...
-    bash ./bin/download_ethics_ds.sh 
-fi
 
-echo Training
-bash bin/train.sh --num_epochs=0 --only_train_head=True  --num_samples_test=1000 --num_samples_train=3000 --ethics_num_epochs=1
