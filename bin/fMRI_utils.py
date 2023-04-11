ffee55dc890ef4ed2fdf5ca5560c67d130882731
Austin <austin.meek10@gmail.com>
2023-04-10 20:31:23 -0400

    start of modifying fmri_utils logic for preproc data

--- bin/fMRI_utils.py
+++ bin/fMRI_utils.py
@@ -19,30 +19,41 @@ from csv import DictReader
 datapath = Path('./data')
 all_scenarios = []
 
-#loop to turn all functional mri data by subject into 2d arrays. This can then be flattened for use in BERT linear layer fine tuning
+
+# loop to turn all functional mri data by subject into 2d arrays. This can then be flattened for use in BERT linear layer fine tuning
 def generate_flattened_data():
-    #download the dataset into the data folder, but don't add it to git
+    # download the dataset into the data folder, but don't add it to git
     fmri_path = datapath / "ds000212"
 
-    #for generating ROI analysis
+    # for generating ROI analysis
     difumo_atlas = nilearn.datasets.fetch_atlas_difumo(dimension=64)
     difumo_masker = nilearn.maskers.NiftiMapsMasker(difumo_atlas['maps'], resampling_target='data', detrend=True).fit()
 
-    #this data structure will allow us to capture anatomical or functional data dependent on run and subject
+    # this data structure will allow us to capture anatomical or functional data dependent on run and subject
     layout = bids.BIDSLayout(fmri_path, config=['bids', 'derivatives'])
     for subject in list(layout.get_subjects()):
         process_subject(subject)
         process_subject_roi(difumo_masker, subject)
 
+
 def process_subject(subject):
+    # TODO Change this so that it only runs on .nii.gz files for which we have valid symlinks, also for variable number of runs
+
     subject_path = datapath / f"ds000212/sub-{subject}/func/"
     target_path = datapath / f"functional_flattened/sub-{subject}"
     target_path.mkdir(parents=True, exist_ok=True)
     run_num = 0
     for bold_f in subject_path.glob("*.nii.gz"):
-        run_num = run_num+1
+        run_num = run_num + 1
         info(f'Processing {bold_f.name}')
-        masked_data = extract_fmri_data(subject, run_num, bold_f)
+
+        # TODO Apply binary brain masks specific to subject, task, and run
+        # core logic should just use nilearn.apply_mask. First parameter is 4d nifti files, second is 3d
+        # binary mask nifti file - true for brain regions
+
+        #masked_data = apply_mask(bold_f, brain_mask)
+
+        # masked_data = extract_fmri_data(subject, run_num, bold_f)
         scenarios = extract_scenarios(subject, run_num, bold_f)
         if masked_data.shape[0] > 0 and len(scenarios) > 0:
             merged = merge_fmri_and_scenarios(masked_data, scenarios)
@@ -53,40 +64,43 @@ def process_subject(subject):
                 events_f = datapath / f"functional_flattened/sub-{subject}/labels-{run_num}.npy"
                 np.save(events_f, scenarios)
 
+
 def merge_fmri_and_scenarios(data, scenarios):
     '''
-    Merges fMRI data with text (scenarios). Masked data in format (time series, voxels). 
-    Ten stories were presented in each 5.5 min run; the total experiment, involving six runs, 
-    lasted 33.2 min. Rest blocks of 10 s were interleaved between each story. 
+    Merges fMRI data with text (scenarios). Masked data in format (time series, voxels).
+    Ten stories were presented in each 5.5 min run; the total experiment, involving six runs,
+    lasted 33.2 min. Rest blocks of 10 s were interleaved between each story.
     See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4769633/
     '''
-    TIME_SERIES_NUM=166
-    SCENARIOS_NUM=10
-    SCENARIO_SEC=22
-    NUM_BEFORE_LAST=1
-    REST_SEC=10
-    #assert data.shape[0] == TIME_SERIES_NUM, f"Expected fMRI time series number {TIME_SERIES_NUM} but {data.shape}"
+    TIME_SERIES_NUM = 166
+    SCENARIOS_NUM = 10
+    SCENARIO_SEC = 22
+    NUM_BEFORE_LAST = 1
+    REST_SEC = 10
+    # assert data.shape[0] == TIME_SERIES_NUM, f"Expected fMRI time series number {TIME_SERIES_NUM} but {data.shape}"
     if not data.shape[0] == TIME_SERIES_NUM:
         info(f"Skipping. Expected fMRI time series number {TIME_SERIES_NUM} but {data.shape}")
-        return 
-    # assert len(scenarios) == SCENARIOS_NUM, f"Expected {SCENARIOS_NUM} scenarios but {len(scenarios)}"
+        return
+        # assert len(scenarios) == SCENARIOS_NUM, f"Expected {SCENARIOS_NUM} scenarios but {len(scenarios)}"
     if not data.shape[0] == TIME_SERIES_NUM:
         info(f"Expected {SCENARIOS_NUM} scenarios but {len(scenarios)}")
         return
     # Add story end time points:
-    nums_filter=[SCENARIO_SEC]
-    for i in range(SCENARIOS_NUM-1):
+    nums_filter = [SCENARIO_SEC]
+    for i in range(SCENARIOS_NUM - 1):
         nums_filter += [nums_filter[-1] + REST_SEC + SCENARIO_SEC]
     # Convert to fMRI time series numbers (taken every 2 sec) (and correct if needed):
-    nums_filter = [e//2 - NUM_BEFORE_LAST for e in nums_filter]
+    nums_filter = [e // 2 - NUM_BEFORE_LAST for e in nums_filter]
     filtered = data[nums_filter, :]
     assert filtered.shape[0] == len(scenarios), f"Expected {filtered.shape[0]=} == {len(scenarios)=}"
     return (filtered, scenarios)
+
 
 def extract_fmri_data(subject, run_num, bold_f):
     bold_symlink_f = str(bold_f.resolve())
     mask_img = compute_epi_mask(bold_symlink_f)
     return apply_mask(bold_symlink_f, mask_img)
+
 
 def process_subject_roi(masker, subject):
     subject_path = datapath / f"ds000212/sub-{subject}/func/"
@@ -94,9 +108,9 @@ def process_subject_roi(masker, subject):
     target_path.mkdir(parents=True, exist_ok=True)
     run_num = 0
     for bold_f in subject_path.glob("*.nii.gz"):
-        run_num = run_num+1
+        run_num = run_num + 1
         info(f'Processing {bold_f.name}')
-        #masked_data = extract_fmri_data(subject, run_num, bold_f)
+        # masked_data = extract_fmri_data(subject, run_num, bold_f)
 
         data = nib.load(bold_f)  # no errors from this !
         roi_time_series = masker.transform(data)
@@ -110,6 +124,7 @@ def process_subject_roi(masker, subject):
                 np.save(filename, data)
                 events_f = datapath / f"ds000212_difumo/sub-{subject}/labels-{run_num}.npy"
                 np.save(events_f, scenarios)
+
 
 def extract_scenarios(subject, run_num, bold_f):
     event_file = bold_f.parent / bold_f.name.replace('_bold.nii.gz', '_events.tsv')
@@ -127,10 +142,12 @@ def extract_scenarios(subject, run_num, bold_f):
                 info(f'Skipping event {event}: no scenario with this item found.')
                 continue
             found = found[0]
-            assert found['type'] == stype, f"Scenario with {item} item does not match the '{stype}' expected type. Scenario: {found}. Event: {event}."
+            assert found[
+                       'type'] == stype, f"Scenario with {item} item does not match the '{stype}' expected type. Scenario: {found}. Event: {event}."
             text = f"{found['background']} {found['action']} {found['outcome']} {found[skind]}"
             scenarios.append(text)
     return scenarios
+
 
 def init_scenarios():
     global all_scenarios
@@ -139,16 +156,20 @@ def init_scenarios():
         for row in reader:
             all_scenarios.append(row)
 
+
 def download_dataset():
     '''
+
+    lost cause, due to Python internals bugging git annex version for datalad. wrote a bash script instead, use that.
+    left here for now, will clean up before PR
 
     :return:
     '''
 
-    #The line directly below will install the entire dataset - around ~260 gigs. Only safe for HPC systems
-    #cmd = 'cd data; datalad install --get-data https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git'
+    # The line directly below will install the entire dataset - around ~260 gigs. Only safe for HPC systems
+    # cmd = 'cd data; datalad install --get-data https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git'
 
-    #this will grab specific files for a test run.
+    # this will grab specific files for a test run.
     ''' 
     List of files to be downloaded and their use:
     sub-XX.html  - html reports generated for each subject detailing fmriprep outlines. If you want to look at the figures
@@ -160,21 +181,22 @@ def download_dataset():
     sub-XX_task_XX_run_XX_space-MNI152NLin2009cAsym_res-2_desk-preproc_bold.json/.nii.gz
         These are the actual bold signals for what we want! Once we perform skull stripping then 
     '''
-    #cmd = 'cd data/ds000212/derivatives; datalad install https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git; mv ds000212-fmriprep fmriprep'
-    #cprocess = run(cmd, shell=True)
+    # cmd = 'cd data/ds000212/derivatives; datalad install https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git; mv ds000212-fmriprep fmriprep'
+    # cprocess = run(cmd, shell=True)
 
-
-    #download specific, individual files here. assuming still in the proper directory (ds000212/derivatives)
+    # download specific, individual files here. assuming still in the proper directory (ds000212/derivatives)
     layout = bids.BIDSLayout('./data/ds000212')
-    for subject in list(layout.get_subjects()): #subject is the number
-        #39 subj, ~240 runs
-        cmd = f'datalad get ./data/ds000212/derivatives/fmriprep/sub-{subject}.html' #datalad here gives git-annex version issue
+    for subject in list(layout.get_subjects()):  # subject is the number
+        # 39 subj, ~240 runs
+        cmd = f'datalad get ./data/ds000212/derivatives/fmriprep/sub-{subject}.html'  # datalad here gives git-annex version issue
         cprocess = run(cmd, shell=True)
 
-    assert cprocess.returncode==0, f'"`{cmd}` failed: {cprocess}'
+    assert cprocess.returncode == 0, f'"`{cmd}` failed: {cprocess}'
+
 
 def info(msg):
     print(msg)
+
 
 event_to_scenario = {
     "A_PHA": ("accidental", "Physical harm"),
@@ -189,12 +211,13 @@ event_to_scenario = {
     "J_NI": ("intentional", "Neutral"),
 }
 
-def main():
-    download_dataset()
-    #init_scenarios()
-    #generate_flattened_data()
 
-    #load out of the numpy files as so
+def main():
+    # download_dataset()
+    # init_scenarios()
+    # generate_flattened_data()
+    print("something")
+    # load out of the numpy files as so
     # test = np.load(datapath / 'functional_flattened/sub-03/1.npy')
     # print(test)
 
