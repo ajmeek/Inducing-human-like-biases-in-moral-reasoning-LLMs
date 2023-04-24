6f855be5f0c3d92587c135ac2505bb6c9bd664f4
Artyom K <artyomkarpov@gmail.com>
2023-04-24 17:07:08 +0300

    Draft of the script

--- run.sh
+++ run.sh
@@ -21,6 +21,9 @@ script_dir="$(dirname "$script_path")"
 readonly script_dir
 root_dir=$( realpath "$script_dir" )
 
+GIT_REMOTE=git@github.com:ameek2/Inducing-human-like-biases-in-moral-reasoning-LLMs.git
+GIT_MAIN_BRANCH_NAME=main
+AISCIBB_GIT_TOKEN=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  
 
 ################################################################################
 
@@ -39,6 +42,30 @@ function datasets() {
 function train() {
     echo Training...
     bash ./bin/train.sh "$@"
+}
+
+function gcp() {
+    if ( printenv AISCIBB_GCP_FLAG > /dev/null ) ; then
+
+        echo At GCP. Running deployment...
+
+        echo Retrieving files.
+        GITBRANCH=${1-$GIT_MAIN_BRANCH_NAME}
+        git clone -b $GITBRANCH $GIT_REMOTE proj
+
+        git clone "https://$YOURTOKEN@github.com/ameek2/Inducing-human-like-biases-in-moral-reasoning-LLMs.git"
+        cd proj
+        
+        echo bash ./run.sh install 
+
+        echo bash ./run.sh datasets 
+
+    else
+        echo Deploying to GCP...
+        [[ "$AISCIBB_GCP_SSH_USERHOST" != "" ]] || ( echo 'Need SSH URL (AISCIBB_GCP_SSH_DESTINATION variable).' ; exit 1 )
+        scp -q ./run.sh scp://$AISCIBB_GCP_SSH_USERHOST/run.sh
+        ssh ssh://$AISCIBB_GCP_SSH_USERHOST 'AISCIBB_GCP_FLAG=1 bash' ./run.sh gcp $( git rev-parse --abbrev-ref HEAD )
+    fi
 }
 
 
