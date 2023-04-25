a5139e40afcef21ca8766efb3b03258f7833c1c4
Artyom K <artyomkarpov@gmail.com>
2023-04-25 19:59:09 +0300

    Docker file fixes

--- run.sh
+++ run.sh
@@ -88,6 +88,7 @@ function gcp() {
         docker container run \
             -e AISCBB_ARTIFACTS_DIR=/aiscbb_artifacts \
             -e AISCBB_DATA_DIR=/asicbb_data \
+            -e TZ=Europe/London \
             -v $AISCBB_ARTIFACTS_DIR:/aiscbb_artifacts \
             -v $AISCBB_DATA_DIR:/asicbb_data \
             -v ~/.gitconfig:/etc/gitconfig \
