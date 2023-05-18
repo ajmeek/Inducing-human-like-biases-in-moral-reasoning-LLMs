b9322e1fa69b0e9679fcff9e8b88bdc0e89a5619
Artyom K <artyomkarpov@gmail.com>
2023-05-18 17:57:02 +0300

    (wip) Changes to run on new GCP

--- bin/datasets.sh
+++ bin/datasets.sh
@@ -2,6 +2,9 @@
 DATADIR=${AISCBB_DATA_DIR:-$root_dir/data}
 [[ -e "$DATADIR" ]] || ( echo "data dir not found: '$DATADIR'"  ; exit 1 )
 
+num_cpus=$( python3 -c "import psutil; print(max(1, psutil.cpu_count(logical=True) - 2))" )
+
+function ethics() { 
 if [[ ! -e $DATADIR/ethics ]]; then 
     echo Downloading and processing ETHICS...
     pushd "$DATADIR"
@@ -13,15 +16,50 @@ if [[ ! -e $DATADIR/ethics ]]; then
     [[ -e "$DATADIR/ethics/commonsense/cm_train.csv" ]] || ( echo 'downloading ethics ds failed'  ; exit 1 )
     echo 'done'
 fi
+}
 
-if [[ ! -e $DATADIR/ds000212  || ! -e $DATADIR/functional_flattened ]]; then 
-    echo Downloading and processing ds000212...
-    ( pushd "$DATADIR" ; datalad install --get-data https://github.com/OpenNeuroDatasets/ds000212.git ; popd )
-
+function ds000212() {
     if [[ ! -e $DATADIR/scenarios.csv ]]; then
         cp $root_dir/data/scenarios.csv $DATADIR/scenarios.csv
     fi
 
-    python3.9 ./bin/fMRI_utils.py
-    echo 'done'
-fi
+    if [[ ! -e $DATADIR/ds000212 ]] ; then
+        echo Downloading ds000212...
+        # TODO : make similar to 
+        datalad install --get-data -s https://github.com/OpenNeuroDatasets/ds000212.git $DATADIR/ds000212
+    fi
+
+    (
+        export TARGET_DS_NAME=ds000212_raw
+        export SOURCE_DS_NAME=ds000212
+        export IS_ROI_ARG=--no-roi
+        echo Running make for $TARGET_DS_NAME
+        make -f ./bin/ds000212.mk all  --jobs $num_cpus --silent
+    )
+    # (
+    #     export TARGET_DS_NAME=ds000212_roi
+    #     export SOURCE_DS_NAME=ds000212
+    #     export IS_ROI_ARG=--roi
+    #     echo Running make for $TARGET_DS_NAME
+    #     make -f ./bin/ds000212.mk all  --jobs $num_cpus  --silent
+    # )
+
+    (
+        exit 0
+        export TARGET_DS_NAME=ds000212_fmriprep
+        export SOURCE_DS_NAME=ds000212/derivatives/ds000212-fmriprep
+        if [[ ! -e $DATADIR/$SOURCE_DS_NAME ]] ; then
+            echo Downloading $DATADIR/$SOURCE_DS_NAME...
+            datalad install -s https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git "$DATADIR/$SOURCE_DS_NAME"
+        fi
+        echo Running make for $TARGET_DS_NAME
+        #make -f ./bin/ds000212prep.mk all  --jobs $num_cpus --silent
+        make -f ./bin/ds000212prep.mk all  --jobs 1 
+    )
+}
+
+
+ethics
+ds000212
+
+echo 'done'
