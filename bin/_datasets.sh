4a297a00e992c03eb37ccc7a7e238680c29261f2
Artyom K <artyomkarpov@gmail.com>
2023-05-23 19:50:33 +0300

    (wip) Consolidating python3, pipenv

--- bin/_datasets.sh
+++ bin/_datasets.sh
@@ -1,36 +1,32 @@
-#!/bin/bash
 
-[[ -e "$pecmd" ]] || ( echo "Unknown pipenv command. Hint: do you run from run.sh?"  ; exit 1 )
-[[ -e "$pycmd" ]] || ( echo "Unknown python command. Hint: do you run from run.sh?"  ; exit 1 )
 [[ -e "$root_dir" ]] || ( echo "root dir not found: '$root_dir'"  ; exit 1 )
-DATADIR=${AISCBB_DATA_DIR:-$root_dir/data}
-[[ -e "$DATADIR" ]] || ( echo "data dir not found: '$DATADIR'"  ; exit 1 )
+[[ -e "$AISCBB_DATADIR" ]] || ( echo "data dir not found: '$AISCBB_DATADIR'"  ; exit 1 )
 
-num_cpus=$( $pycmd -c "import psutil; print(max(1, psutil.cpu_count(logical=True) - 2))" )
+num_cpus=$( pycmd -c "import psutil; print(max(1, psutil.cpu_count(logical=True) - 2))" )
 
 function ethics() { 
-if [[ ! -e $DATADIR/ethics ]]; then 
+if [[ ! -e $AISCBB_DATADIR/ethics ]]; then 
     echo Downloading and processing ETHICS...
-    pushd "$DATADIR"
+    pushd "$AISCBB_DATADIR"
     [[ -e ethics.tar ]] || curl -O 'https://people.eecs.berkeley.edu/~hendrycks/ethics.tar' 
     mkdir -p ethics
     tar -xvf ethics.tar 
     rm ./ethics.tar
     popd
-    [[ -e "$DATADIR/ethics/commonsense/cm_train.csv" ]] || ( echo 'downloading ethics ds failed'  ; exit 1 )
+    [[ -e "$AISCBB_DATADIR/ethics/commonsense/cm_train.csv" ]] || ( echo 'downloading ethics ds failed'  ; exit 1 )
     echo 'done'
 fi
 }
 
 function ds000212() {
-    if [[ ! -e $DATADIR/ds000212_scenarios.csv ]]; then
-        cp $root_dir/data/ds000212_scenarios.csv $DATADIR/ds000212_scenarios.csv
+    if [[ ! -e $AISCBB_DATADIR/ds000212_scenarios.csv ]]; then
+        cp $root_dir/data/ds000212_scenarios.csv $AISCBB_DATADIR/ds000212_scenarios.csv
     fi
 
-    if [[ ! -e $DATADIR/ds000212 ]] ; then
+    if [[ ! -e $AISCBB_DATADIR/ds000212 ]] ; then
         echo Downloading ds000212...
         # TODO : make similar to 
-        $pecmd datalad install --get-data -s https://github.com/OpenNeuroDatasets/ds000212.git $DATADIR/ds000212
+        pecmd datalad install --get-data -s https://github.com/OpenNeuroDatasets/ds000212.git $AISCBB_DATADIR/ds000212
     fi
 
     (
@@ -51,9 +47,9 @@ function ds000212() {
     #(
     #    export TARGET_DS_NAME=ds000212_fmriprep
     #    export SOURCE_DS_NAME=ds000212/derivatives/ds000212-fmriprep
-    #    if [[ ! -e $DATADIR/$SOURCE_DS_NAME ]] ; then
-    #        echo Downloading $DATADIR/$SOURCE_DS_NAME...
-    #        datalad install -s https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git "$DATADIR/$SOURCE_DS_NAME"
+    #    if [[ ! -e $AISCBB_DATADIR/$SOURCE_DS_NAME ]] ; then
+    #        echo Downloading $AISCBB_DATADIR/$SOURCE_DS_NAME...
+    #        datalad install -s https://github.com/OpenNeuroDerivatives/ds000212-fmriprep.git "$AISCBB_DATADIR/$SOURCE_DS_NAME"
     #    fi
     #    echo Running make for $TARGET_DS_NAME
     #    #make -f ./bin/ds000212prep.mk all  --jobs $num_cpus --silent
--- run.sh
+++ run.sh
@@ -26,119 +26,34 @@ export GIT_REMOTE=github.com/ameek2/Inducing-human-like-biases-in-moral-reasonin
 export AISCBB_ARTIFACTS_DIR=${AISCBB_ARTIFACTS_DIR:-$root_dir/artifacts}
 export AISCBB_DATA_DIR=${AISCBB_DATA_DIR:-$root_dir/data}
 
-export pycmd=$( which python3.10 )
-export pecmd=pipenv --python $pycmd
+function pycmd { 
+    $( which python3 ) $@ 
+}
+function pecmd { 
+    pipenv $@
+}
 
 ################################################################################
 
 function provision() {
-    $pecmd install
+    pycmd -m pip install pipenv 
+
+    pecmd install
+
+    pycmd --user -m pip install datalad-installer
+    datalad-installer --sudo ok git-annex -m datalad/git-annex:release
 }
 
 function datasets() {
-    echo Preparing datasets...
-    source ./bin/datasets.sh
+    source ./bin/_datasets.sh
+}
+
+function gcp() {
+    source ./bin/_gcp.sh
 }
 
 function train() {
-    echo Training...
-    pushd $root_dir > /dev/null
-
-    export PJRT_DEVICE=TPU
-    $pecmd run "$root_dir/src/main.py" "$@"  || popd
-}
-
-# TODO: refactor, move to its file.
-function gcp() {
-    GCPUSAGE='Runs tasks at Google Cloud Platform.
-Provide task after gcp like this: run.sh gcp my-task [parameter ...].
-If no tasks provided (run.sh gcp) it syncs remote and local files (gets results).'
-
-    if [[ -z ${AISCIBB_GCP_FLAG-} ]] ; then
-        echo $GCPUSAGE
-        # In local environment.
-        [[ ! -z ${AISCIBB_GIT_TOKEN-} ]]  || ( echo "Please set AISCIBB_GIT_TOKEN environment variable (see https://github.com/settings/tokens)." ; exit 1 )
-        [[ ! -z ${AISCIBB_GCP_SSH_USERHOST-} ]]  || ( echo "Please set AISCIBB_GCP_SSH_USERHOST environment variable (example: user@123.123.123.123)." ; exit 1 )
-
-        echo Uploading files required to run commands...
-        scp -q ~/.gitconfig scp://$AISCIBB_GCP_SSH_USERHOST/.gitconfig
-        scp -q ./run.sh scp://$AISCIBB_GCP_SSH_USERHOST/run.sh
-        if [[ $# -gt 0 ]]; then
-            echo Calling GCP to run a task...
-            ssh ssh://$AISCIBB_GCP_SSH_USERHOST 'AISCIBB_GCP_FLAG=1 bash' ./run.sh gcp $( git rev-parse --abbrev-ref HEAD ) "$AISCIBB_GIT_TOKEN" "$@"
-        fi
-
-        echo Getting results
-        ssh ssh://$AISCIBB_GCP_SSH_USERHOST 'AISCIBB_GCP_FLAG=1 bash' ./run.sh gcp
-
-        echo Syncing artifacts from GCP...
-        # TODO: configure artifacts dir at remote.
-        rsync --info=progress2 -r $AISCIBB_GCP_SSH_USERHOST:~/artifacts/\* $AISCBB_ARTIFACTS_DIR
-        echo Done. Artifacts at $AISCBB_ARTIFACTS_DIR
-    else
-        # In remote environment.
-        # If all prerequisits met:
-        mkdir -vp $AISCBB_ARTIFACTS_DIR
-        mkdir -vp $AISCBB_DATA_DIR
-
-        C_ID_FILE=./aiscbb_container_id.cid
-        if [[ $# -eq 0 ]]; then
-            echo "Containers (tasks):"
-            sudo docker ps
-            if [[ -e $C_ID_FILE ]]; then
-                CID=$( cat $C_ID_FILE ) 
-                if sudo docker ps | grep $CID ; then 
-                    C_LOG_FILE="$AISCBB_ARTIFACTS_DIR/$( date +%Y-%m-%d-%H%M )_run_sh.log "
-                    sudo docker logs -f $CID |& tee $C_LOG_FILE
-                fi
-            fi
-        else
-            echo [GCP] Run a task...
-            echo [GCP] Stoping current task if any.
-            if [[ -e $C_ID_FILE ]] ; then 
-                CID=$( cat $C_ID_FILE ) 
-                if sudo docker ps | grep $CID ; then 
-                    echo There is $CID container running. Stoppping...
-                    C_LOG_FILE="$AISCBB_ARTIFACTS_DIR/$( date +%Y-%m-%d-%H%M )_run_sh.log "
-                    sudo docker logs -f $CID &> $C_LOG_FILE
-                    sudo docker rm $CID
-                fi
-                rm $C_ID_FILE
-            fi
-            
-            echo Clonning repository...
-            GITBRANCH=${1-$GIT_MAIN_BRANCH_NAME}
-            AISCIBB_GIT_TOKEN=$2
-            TARGETDIR=~/aiscbbproj
-            if [[ -e $TARGETDIR ]] ; then 
-                echo Removing old repo directory
-                rm -rf $TARGETDIR
-            fi
-            git clone -b $GITBRANCH "https://$AISCIBB_GIT_TOKEN@$GIT_REMOTE" $TARGETDIR 
-
-            echo Building docker image...
-            ( cd $TARGETDIR ; sudo docker buildx build -t aiscbb . )
-
-            echo Running container...
-            shift 2  # Remove first two params for gcp.
-
-            sudo docker container run \
-                -e AISCBB_ARTIFACTS_DIR=/aiscbb_artifacts \
-                -e AISCBB_DATA_DIR=/asicbb_data \
-                -v $AISCBB_ARTIFACTS_DIR:/aiscbb_artifacts \
-                -v $AISCBB_DATA_DIR:/asicbb_data \
-                -v ~/.gitconfig:/etc/gitconfig \
-                --cidfile="$C_ID_FILE" \
-                --detach \
-                aiscbb bash run.sh "$@"
-            CID=$( cat $C_ID_FILE )
-            C_LOG_FILE="$AISCBB_ARTIFACTS_DIR/$( date +%Y-%m-%d-%H%M )_run_sh.log "
-            sudo docker logs -f $CID |& tee $C_LOG_FILE
-
-            [[ ! -e %TARGETDIR ]] || rm -dr $TARGETDIR
-            echo At GCP. Finished.
-        fi
-    fi
+    pecmd run "$root_dir/src/main.py" "$@"
 }
 
 ################################################################################
