e4135241c360f62ae9a80d4b2e97072ab7f04134
Artyom K <artyomkarpov@gmail.com>
2023-07-14 19:26:38 +0300

    Introducing paramater
    Using constants instead of intervals
    
    Connecting var sampling methods
    
    Usage for _datasets.sh
    
    save
    
    Refactor: turning config into context
    
    Refactor: turning config into context
    
    Refactor: turning config into context
    
    Refactor: encapsulating datasets logic
    
    Refactor: encapsulating datasets logic
    
    Refactor: encapsulating datasets logic
    
    Constants
    
    Enum for Sampling
    
    Fixing broken pieces
    
    (wip) fixing bug

--- src/main.py
+++ src/main.py
@@ -1,34 +1,27 @@
-import torch as t
-from torch.utils.data import TensorDataset
-from transformers import AutoTokenizer, AutoModel
-from utils.loading_data import load_ethics_ds, multiple_dataset_loading
-from utils.preprocessing import preprocess_prediction
-from model import BERT
-from pl_model import LitBert
-import lightning.pytorch as pl
-from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
-from pathlib import Path
-import pandas as pd
-import numpy as np
-import argparse
-from os import environ
 from datetime import datetime
+from lightning.pytorch.loggers import WandbLogger
+from model import BERT
+from os import environ
+from pathlib import Path
+from pl_model import LitBert
+from pprint import pprint
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoModel
+from utils.EthicsDataset import EthicsDataset
+from utils.constants import Sampling
+from utils.loading_data import multiple_dataset_loading, DEFAULT_DATASETS
+
+import argparse
+import lightning.pytorch as pl
 import wandb
 
-from pprint import pprint, pformat
-
-datapath = Path(environ.get('AISCBB_DATA_DIR','./data'))
-artifactspath = Path(environ.get('AISCBB_ARTIFACTS_DIR','./artifacts'))
-
-def train(config):
-    assert datapath.exists(), 'Expected data dir present.'
-    artifactspath.mkdir(exist_ok=True)
-    pprint('Config:')
-    pprint(config, indent=2)
+def train(context):
+    pprint('Context:')
+    pprint(context, indent=2)
     # Define the tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained(config['checkpoint'])
+    tokenizer = AutoTokenizer.from_pretrained(context['checkpoint'])
     # TODO make sure it doesn't add SEP tokens when there's a full stop
-    base_model = AutoModel.from_pretrained(config['checkpoint'])
+    base_model = AutoModel.from_pretrained(context['checkpoint'])
 
     #use_ia3_layers = False
     # if use_ia3_layers:
@@ -37,45 +30,41 @@ def train(config):
     #     base_model = modify_with_ia3(base_model, layers_to_replace_with_ia3)
 
     # Load the dataset
-    dataloaders, train_head_dims = multiple_dataset_loading(datapath, tokenizer, config)
+    dataloaders, train_head_dims = multiple_dataset_loading(tokenizer, context)
 
     # Define the model
     model = BERT(
         base_model,
         head_dims=train_head_dims
     )
-    lit_model = LitBert(model, config)
+    lit_model = LitBert(model, context)
 
-    logger = WandbLogger(save_dir=artifactspath, project="AISC_BB")
-    logger.log_hyperparams(config)
+    logger = WandbLogger(save_dir=context['artifactspath'], project="AISC_BB")
+    logger.log_hyperparams(context)
 
     # train the model
     trainer = pl.Trainer(
-        limit_train_batches=config['batches_per_epoch'],
-        max_epochs=config['num_epochs'],
+        limit_train_batches=context['batches_per_epoch'],
+        max_epochs=context['num_epochs'],
         accelerator='auto',
         devices='auto',
         strategy='auto',
         logger=logger,
         log_every_n_steps=1,
-        default_root_dir=artifactspath,
+        default_root_dir=context['artifactspath'],
         enable_checkpointing=False  # Avoid saving full model into a disk (GBs)
     )
     print('Fine tuning BERT...')
     trainer.fit(lit_model, dataloaders)
 
     # Test the model
-    test_loader, _ = load_ethics_ds(
-        datapath,
-        tokenizer,
-        config,
-        is_train=False
-    )
+    data = EthicsDataset(context, tokenizer, is_train=False)
+    test_loader = DataLoader( data, batch_size=context['batch_size'], shuffle=context['shuffle_test'])
     print('Testing on ETHICS...')
     trainer.test(lit_model, dataloaders=test_loader)
     logger.save()
     wandb.finish()
-    trainer.save_checkpoint(artifactspath / f'model-{datetime.utcnow().isoformat(timespec="minutes").replace(":","")}.ckpt')
+    trainer.save_checkpoint(context['artifactspath'] / f'model-{datetime.utcnow().isoformat(timespec="minutes").replace(":","")}.ckpt')
 
 
 def get_config():
@@ -103,6 +92,7 @@ def get_args() -> argparse.ArgumentParser:
     """Get command line arguments"""
 
     parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='run model training'
     )
     parser.add_argument(
@@ -110,49 +100,42 @@ def get_args() -> argparse.ArgumentParser:
         default='1',
         type=int,
         help='Number of epochs to fine tune a model on fMRI data.'
-             '(default: 1)'
     )
     parser.add_argument(
         '--batches_per_epoch',
         default='15',
         type=int,
         help='Batches per epoch.'
-             '(default: 1)'
     )
     parser.add_argument(
         '--batch_size',
         default='15',
         type=int,
         help='Batch size.'
-             '(default: 15)'
     )
     parser.add_argument(
         '--regularize_from_init',
         default='False',
         type=str,
         help='Regularize from init (base) model.'
-             '(default: True)'
     )
     parser.add_argument(
         '--regularization_coef',
         default='0.1',
         type=float,
         help='Regularization from init coef.'
-             '(default: 0.1)'
     )
     parser.add_argument(
         '--num_samples_train',
         default='100',
         type=int,
         help='Number of train samples (fine tuning).'
-             '(default: 100)'
     )
     parser.add_argument(
         '--num_samples_test',
         default='100',
         type=int,
         help='Number of test samples.'
-             '(default: 64)'
     )
     parser.add_argument(
         '--shuffle_train',
@@ -171,21 +154,19 @@ def get_args() -> argparse.ArgumentParser:
         default='True',
         type=str,
         help='Train only attached head.'
-             '(default: True)'
     )
     parser.add_argument(
         '--checkpoint',
         default='bert-base-cased',
         type=str,
         help='HuggingFace model.'
-             '(default: bert-base-cased)'
     )
     parser.add_argument(
         '--train_datasets',
         nargs='+',
-        default=['ethics', 'ds000212'],
+        default=DEFAULT_DATASETS,
         type=str,
-        help='Datasets to train on. This can be multiple datasets, e.g. "ds000212 ethics/...".'
+        help='Datasets to train on.'
     )
     parser.add_argument(
         '--loss_names',
@@ -207,11 +188,31 @@ def get_args() -> argparse.ArgumentParser:
         type=float,
         help='Learning rate'
     )
-
+    parser.add_argument(
+        '--sampling_method',
+        default=Sampling.LAST.name,
+        choices=Sampling,
+        type=lambda v: Sampling[v.replace('Sampling.', '')],
+        help='Method for sampling fMRI data.'
+    )
+    parser.add_argument(
+        '--datapath',
+        default=Path(environ.get('AISCBB_DATA_DIR','./data')),
+        type=str,
+        help='Path to the folder with datasets.'
+    )
+    parser.add_argument(
+        '--artifactspath',
+        default=Path(environ.get('AISCBB_ARTIFACTS_DIR','./artifacts')),
+        type=str,
+        help='Path to the folder for artifacts.'
+    )
     return parser
 
 
 if __name__ == '__main__':
-    config = get_config()
-    train(config)
+    context = get_config()
+    assert context['datapath'].exists(), 'Expected data dir present.'
+    context['artifactspath'].mkdir(exist_ok=True)
+    train(context)
     print('Done')
---
+++ src/utils/constants.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from enum import Flag, auto
+
+class Sampling(Flag):
+    LAST = auto()
+    AVG = auto()
+    MIDDLE = auto()
+    SENTENCES = auto()
+    ONE_POINT_METHODS = LAST | AVG | MIDDLE
+
+@dataclass(frozen=True)
+class FMRI:
+    TR = 2
+    REACT_TIME = 3 // TR
+
+@dataclass(frozen=True)
+class DS000212:
+    @dataclass(frozen=True)
+    class Periods:
+        BACKGROUND = 6
+        ACTION = 4
+        OUTCOME = 4
+        INTENT = 4
+        JUDGMENT = 4
+        ENDS = [
+            BACKGROUND // FMRI.TR,
+            ACTION // FMRI.TR,
+            OUTCOME // FMRI.TR,
+            INTENT // FMRI.TR,
+            JUDGMENT // FMRI.TR
+        ]
