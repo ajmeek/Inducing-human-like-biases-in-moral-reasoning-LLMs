f4d14ea812c1384208765cd944fbf8ab33e31627
Artyom K <artyomkarpov@gmail.com>
2023-08-09 10:26:56 +0300

    Constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
 class Sampling:
     LAST = 'last'
     AVG = 'avg'
@@ -7,11 +9,14 @@ class Sampling:
     METHODS = [ LAST, AVG, MIDDLE, SENTENCES ]
     ONE_POINT_METHODS = [ LAST, AVG, MIDDLE ]
 
+@dataclass(frozen=True)
 class FMRI:
     TR = 2
     REACT_TIME = 3 // TR
 
+@dataclass(frozen=True)
 class DS000212:
+    @dataclass(frozen=True)
     class Periods:
         BACKGROUND = 6
         ACTION = 4
