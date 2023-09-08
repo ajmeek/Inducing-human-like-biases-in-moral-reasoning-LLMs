d5970f32e4adc3134ef07dfd195df98c2cb7638c
Artyom K <artyomkarpov@gmail.com>
2023-09-08 15:17:03 +0300

    Checkpointing

--- bin/train_bert_large_cased.sh
+++ bin/train_bert_large_cased.sh
@@ -3,18 +3,18 @@
     --ds1.name commonsense \
     --ds1.test.slicing '[:0]' \
     --ds1.train.batch_size 5 \
-    --ds1.train.slicing '[:100%]' \
+    --ds1.train.slicing '[:2000]' \
     --ds1.validation.batch_size 5 \
-    --ds1.validation.slicing '[:100%]' \
+    --ds1.validation.slicing '[:1000]' \
     --ds2.train.batch_size 5 \
-    --has_ReduceLROnPlateau False \
-    --lr 2e-3 \
+    --has_ReduceLROnPlateau True \
+    --lr 1e-4 \
     --lr_scheduler_frequency 10 \
     --lr_scheduler_interval epoch \
     --max_epochs 1000 \
     --model_path 'bert-large-cased' \
     --sampling_method SENTENCES \
-    --to_save_model True \
+    --enable_checkpointing True \ 
     --train_all True
 
 ./run.sh train \
@@ -22,16 +22,19 @@
     --ds1.name commonsense \
     --ds1.test.slicing '[:0]' \
     --ds1.train.batch_size 5 \
-    --ds1.train.slicing '[:100%]' \
+    --ds1.train.slicing '[:2000]' \
     --ds1.validation.batch_size 5 \
-    --ds1.validation.slicing '[:100%]' \
+    --ds1.validation.slicing '[:1000]' \
     --ds2.enable False \
-    --has_ReduceLROnPlateau False \
-    --lr 2e-3 \
+    --has_ReduceLROnPlateau True \
+    --lr 1e-4 \
     --lr_scheduler_frequency 10 \
     --lr_scheduler_interval epoch \
     --max_epochs 1000 \
     --model_path 'bert-large-cased' \
     --sampling_method SENTENCES \
-    --to_save_model False \
+    --enable_checkpointing True \ 
     --train_all True
+
+
+vastai stop instance $CONTAINER_ID;
