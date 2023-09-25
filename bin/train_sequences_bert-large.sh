e2082ad63428416a0cf7a7bc02cfd2ae7a5dea78
Artyom <artyomkarpov@gmail.com>
2023-09-25 13:12:32 +0000

    experiment

--- bin/train_sequences_bert-large.sh
+++ bin/train_sequences_bert-large.sh
@@ -5,19 +5,20 @@ echo "First train only a head on Ethics"
 ./run.sh train \
     --check_val_every_n_epoch 3 \
     --ds1.name commonsense \
-    --ds1.test.slicing '[:1000]' \
+    --ds1.test.slicing '[:2000]' \
     --ds1.train.batch_size 6 \
     --ds1.train.slicing '[:6000]' \
     --ds1.validation.batch_size 5 \
-    --ds1.validation.slicing '[:2000]' \
+    --ds1.validation.slicing '[:3000]' \
+    --ds1.validation.shuffle 1 \
     --ds2.enable False \
-    --max_epochs 30 \
+    --max_epochs 40 \
     --model_path 'bert-large-cased' \
-    --before_lr_decay_warm_up_steps 100 \
+    --before_lr_decay_warm_up_steps 2000 \
     --find_learning_rate 1 \
-    --accumulate_grad_batches 5 \
+    --accumulate_grad_batches 10 \
     --stepLR_gamma 0.99 \
-    --stepLR_step_size 10 \
+    --stepLR_step_size 100 \
     --train_all False \
     --last_checkpoint_path artifacts/train_head_on_ethics_bert_large.ckpt
 
@@ -26,11 +27,12 @@ echo "Now train all on fMRI only and see if Ethics accuracy improves"
 ./run.sh train \
     --check_val_every_n_epoch 3 \
     --ds1.name commonsense \
-    --ds1.test.slicing '[:1000]' \
+    --ds1.test.slicing '[:2000]' \
     --ds1.train.batch_size 6 \
     --ds1.train.slicing '[:0]' \
     --ds1.validation.batch_size 5 \
-    --ds1.validation.slicing '[:2000]' \
+    --ds1.validation.slicing '[:3000]' \
+    --ds1.validation.shuffle 1 \
     --ds2.enable True \
     --ds2.name LFB-AVG \
     --ds2.train.slicing '[:100%]' \
@@ -40,10 +42,11 @@ echo "Now train all on fMRI only and see if Ethics accuracy improves"
     --ds2.train.batch_size 4 \
     --max_epochs 100 \
     --model_path 'bert-large-cased' \
-    --before_lr_decay_warm_up_steps 100 \
+    --before_lr_decay_warm_up_steps 2000 \
     --find_learning_rate 1 \
-    --accumulate_grad_batches 100 \
+    --accumulate_grad_batches 10 \
     --stepLR_gamma 0.99 \
-    --stepLR_step_size 10 \
+    --stepLR_step_size 100 \
     --train_all True \
-    --checkpoint_path artifacts/train_head_on_ethics_bert_large.ckpt
+    --checkpoint_path artifacts/train_head_on_ethics_bert_large.ckpt \
+    --last_checkpoint_path artifacts/train_head_on_ethics_bert_large_end.ckpt
