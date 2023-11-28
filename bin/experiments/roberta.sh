fac3969afc51dcbec3ab854f44f00b385855daba
Artyom K. <artyomkarpov@gmail.com>
2023-11-28 15:21:30 +0300

    experiment script

--- bin/experiments/roberta.sh
+++ bin/experiments/roberta.sh
@@ -2,46 +2,49 @@
 #
 # Runs experiments on RoBERTa.
 # https://huggingface.co/roberta-large
-ACC_GB=10
-BS=8
+
+# Default experiment parameters:
 DS2_TRAIN_SLICE='[:1000]'
 ETHICS_EPOCHS=15
-FBS=0
-FLR=0
 FMRI_EPOCHS=55
-LR=1e-5
-LRBMF=1.0
 MODEL_PATH=roberta-large
 WARM_UP=0.5
+SAMPLING=LAST
+ENAME=UNKNOWN
 
 function experiment() {
+    # Tunnel all through here.
+    #
+    WANDB_NAME="${MODEL_PATH}_${ENAME}_$(date +%y-%m-%d_%H%M)"
+    export WANDB_NAME
     echo ===============================================================
-    echo $MODEL_PATH $( date ) 
+    echo "$WANDB_NAME" 
     echo ===============================================================
+    LAST_CKPT="artifacts/${WANDB_NAME}.ckpt"
     ./run.sh train \
-        --accumulate_grad_batches $ACC_GB \
-        --batch_size_all $BS \
+        --accumulate_grad_batches 10 \
+        --batch_size_all 8 \
         --check_val_every_n_epoch 2 \
-        --checkpoint_path $CKPT_FILE \
+        --checkpoint_path "$CKPT_FILE" \
         --ds1.name commonsense \
         --ds1.test.slicing '[:1000]' \
         --ds1.train.shuffle 1 \
-        --ds1.train.slicing $DS1_TRAIN_SLICE \
+        --ds1.train.slicing "$DS1_TRAIN_SLICE" \
         --ds1.validation.shuffle 0 \
         --ds1.validation.slicing '[:1000]' \
         --ds2.enable $ds2enable \
-        --ds2.name LFB-$SAMPLING \
+        --ds2.name "LFB-$SAMPLING" \
         --ds2.test.slicing '[:100%]' \
         --ds2.train.shuffle 1 \
-        --ds2.train.slicing $DS2_TRAIN_SLICE \
-        --find_bs $FBS \
-        --find_lr $FLR \
-        --last_checkpoint_path $LAST_CKPT \
-        --lr $LR \
-        --lr_base_model_factor $LRBMF \
+        --ds2.train.slicing "$DS2_TRAIN_SLICE" \
+        --find_bs 0 \
+        --find_lr 0 \
+        --last_checkpoint_path "$LAST_CKPT" \
+        --lr 1e-5 \
+        --lr_base_model_factor 1.0 \
         --lr_warm_up_steps $WARM_UP \
         --max_epochs $MAX_EPOCHS \
-        --model_path $MODEL_PATH \
+        --model_path "$MODEL_PATH" \
         --num_workers 0 \
         --profiler simple \
         --stepLR_gamma 0.99 \
@@ -51,88 +54,72 @@ function experiment() {
 
 }
 
+#########################################
+# No fine tuning
+
+ENAME="no_F_T"
+CKPT_FILE=
+DS1_TRAIN_SLICE="[:80%]"
+DS2_TRAIN_SLICE="[:80%]"
+MAX_EPOCHS=0
+TRAIN_ALL=0
+ds2enable=0
+experiment
+
 
 #########################################
-# export WANDB_NAME="$MODEL_PATH, no f.t. $(date +%y-%m-%d\ %H:%M)"
-# echo $WANDB_NAME
-# CKPT_FILE=
-# DS1_TRAIN_SLICE="[:80%]"
-# DS2_TRAIN_SLICE="[:80%]"
-# LAST_CKPT=
-# MAX_EPOCHS=0
-# TRAIN_ALL=0
-# ds2enable=0
-# experiment
-# 
-# 
-# #########################################
-# export WANDB_NAME="$MODEL_PATH, (fmri and Ethics)-HM $(date +%y-%m-%d\ %H:%M)"
-# echo $WANDB_NAME
-# 
-# CKPT_FILE=
-# DS1_TRAIN_SLICE="[:80%]"
-# DS2_TRAIN_SLICE="[:80%]"
-# LAST_CKPT=
-# MAX_EPOCHS=7
-# WARM_UP=1.0
-# TRAIN_ALL=1
-# ds2enable=0
-# experiment
+# fMRI and ETHICS 
 
+ENAME="fmri_and_ethics_hm"
+CKPT_FILE=
+DS1_TRAIN_SLICE="[:80%]"
+DS2_TRAIN_SLICE="[:80%]"
+MAX_EPOCHS=7
+TRAIN_ALL=1
+ds2enable=1
+OLD_WARM_UP=$WARM_UP
+WARM_UP=1.0
+experiment
 
-WARM_UP=0.5
-TRANSFER_CKPT=artifacts/RoBERTa-transfer.ckpt
-
-#########################################
-# export WANDB_NAME="$MODEL_PATH, fmri-HM then Ethics-HM $(date +%y-%m-%d\ %H:%M)"
-# echo $WANDB_NAME
-# 
-# #echo First all on fMRI:
-# CKPT_FILE=
-# DS1_TRAIN_SLICE="[:0]"
-# DS2_TRAIN_SLICE="[:1000]"
-# LAST_CKPT=$TRANSFER_CKPT
-# MAX_EPOCHS=$FMRI_EPOCHS
-# SAMPLING=AVG
-# TRAIN_ALL=1
-# ds2enable=1
-# experiment
-# 
-# #echo Second, train on ETHICS only:
-# export WANDB_NAME="$MODEL_PATH, fmri-HM then Ethics-HM $(date +%y-%m-%d\ %H:%M)"
-# CKPT_FILE=$TRANSFER_CKPT
-# DS1_TRAIN_SLICE="[:80%]"
-# DS2_TRAIN_SLICE="[:0]"
-# LAST_CKPT=
-# MAX_EPOCHS=20
-# TRAIN_ALL=1
-# ds2enable=0
-# experiment
-
+WARM_UP=$OLD_WARM_UP
 
 #########################################
 # Ethics-HM then fmri-HM
 
-export WANDB_NAME="$MODEL_PATH, Ethics-HM then fmri-HM  $(date +%y-%m-%d\ %H:%M)"
-echo $WANDB_NAME
-
-#echo First all on Ethics
+ENAME="Ethics-HM_then_fmri-HM"
 CKPT_FILE=
 DS1_TRAIN_SLICE="[:80%]"
-LAST_CKPT=$TRANSFER_CKPT
 MAX_EPOCHS=$ETHICS_EPOCHS
 TRAIN_ALL=1
 ds2enable=0
 experiment
 
-echo Second, train on fMRI only, while testing on Ethics:
-CKPT_FILE=$TRANSFER_CKPT
+CKPT_FILE="$LAST_CKPT"
 DS1_TRAIN_SLICE="[:0]"
-LAST_CKPT=
 MAX_EPOCHS=$FMRI_EPOCHS
-SAMPLING=LAST
 TRAIN_ALL=1
 ds2enable=1
+experiment
+
+#########################################
+# fMRI then ETHICS
+
+ENAME="fmri-hm_then_ethics-hm"
+CKPT_FILE=
+DS1_TRAIN_SLICE="[:0]"
+DS2_TRAIN_SLICE="[:1000]"
+MAX_EPOCHS=$FMRI_EPOCHS
+TRAIN_ALL=1
+ds2enable=1
+experiment
+
+CKPT_FILE="$LAST_CKPT"
+DS1_TRAIN_SLICE="[:80%]"
+DS2_TRAIN_SLICE="[:0]"
+LAST_CKPT=
+MAX_EPOCHS=$ETHICS_EPOCHS
+TRAIN_ALL=1
+ds2enable=0
 experiment
 
 
