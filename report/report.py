6c54cdda4e3b59016d245639426a9fbb6d810f76
Artyom K. <artyomkarpov@gmail.com>
2023-12-19 19:20:27 +0300

    save

--- report/report.py
+++ report/report.py
@@ -16,11 +16,11 @@ pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
 # %%
 # Project is specified by <entity/project-name>
-runs = api.runs("asdfasdfasdfdsafsd/AISC_BB")
+runs_data = api.runs("asdfasdfasdfdsafsd/AISC_BB")
 
 summary_list, config_list, name_list = [], [], []
 data_list = []
-for run in runs:
+for run in runs_data:
     if "_runtime" not in run.summary:
         continue
     # .summary contains the output keys/values for metrics like accuracy.
@@ -40,24 +40,24 @@ for run in runs:
     c = {k: v for k, v in run.config.items() if not k.startswith("_")}
     data_list.append(s | c | {"name": run.name} | {'tags': run.tags})
 
-runs_df = pd.DataFrame(data_list)
+rdf = pd.DataFrame(data_list)  # Runs DataFrame.
 
 # %%
 # Sort columns by name:
-runs_df = runs_df.reindex(sorted(runs_df.columns), axis=1)
+rdf = rdf.reindex(sorted(rdf.columns), axis=1)
 # %%
-runs_df.to_csv("report/project_original.csv")
+rdf.to_csv("report/project_original.csv")
 
 # %%
 # Load from csv:
-runs_df = pd.read_csv("report/project_original.csv")
+rdf = pd.read_csv("report/project_original.csv")
 
 # %%
 # Include only those after 1 Sep 2023:
-runs_df = runs_df[runs_df["_timestamp"] >= datetime(2023, 9, 1).timestamp()]
+rdf = rdf[rdf["_timestamp"] >= datetime(2023, 9, 1).timestamp()]
 # %% 
 # Get those items in runs_df that has 'bad-hyperparams' tag
-runs_df = runs_df[runs_df['tags'].apply(lambda x: 'bad-hyperparams' not in x)]
+rdf = rdf[rdf['tags'].apply(lambda x: 'bad-hyperparams' not in x)]
 
 # %%
 # Clear extra quotes in 'checkpoint_path' and 'last_checkpoint_path' columns:
@@ -67,12 +67,12 @@ def clear_quotes(s):
     return s.strip("\"'")
 
 
-runs_df["checkpoint_path"] = runs_df["checkpoint_path"].apply(clear_quotes)
-runs_df["last_checkpoint_path"] = runs_df["last_checkpoint_path"].apply(clear_quotes)
+rdf["checkpoint_path"] = rdf["checkpoint_path"].apply(clear_quotes)
+rdf["last_checkpoint_path"] = rdf["last_checkpoint_path"].apply(clear_quotes)
 
 # %%
 # Merge results for Commonsense Hard Test set:
-runs_df["cs_hard_set_acc"] = np.nan
+rdf["cs_hard_set_acc"] = np.nan
 for column in [
     "commonsense-test-MulticlassAccuracy",
     "commonsense-test-label-MulticlassAccuracy",
@@ -83,7 +83,7 @@ for column in [
     "test-commonsense-acc/dataloader_idx_0",
     "test_acc",
 ]:
-    runs_df["cs_hard_set_acc"] = runs_df["cs_hard_set_acc"].fillna(runs_df[column])
+    rdf["cs_hard_set_acc"] = rdf["cs_hard_set_acc"].fillna(rdf[column])
 
 # %%
 # Merge results for Commonsense Test set:
@@ -95,40 +95,24 @@ columns_to_merge = [
     "validation-MulticlassAccuracy",
     "validation-commonsense-acc",
 ]
-runs_df["cs_test_set_acc"] = np.nan
+rdf["cs_test_set_acc"] = np.nan
 for column in columns_to_merge:
-    runs_df["cs_test_set_acc"] = runs_df["cs_test_set_acc"].fillna(runs_df[column])
+    rdf["cs_test_set_acc"] = rdf["cs_test_set_acc"].fillna(rdf[column])
 
 # %% 
 # Remove rows with wrong accuracy, not in [0, 1] for cs_hard_set_acc and cs_test_set_acc:
-runs_df = runs_df[runs_df["cs_hard_set_acc"].between(0, 1)]
-runs_df = runs_df[runs_df["cs_test_set_acc"].between(0, 1)]
+rdf = rdf[rdf["cs_hard_set_acc"].between(0, 1)]
+rdf = rdf[rdf["cs_test_set_acc"].between(0, 1)]
 # %%
-runs_df["model_path"] = runs_df["model_path"].fillna(runs_df["checkpoint"])
+rdf["model_path"] = rdf["model_path"].fillna(rdf["checkpoint"])
 
 # %%
-runs_df["timestamp"] = runs_df["_timestamp"].apply(
+rdf["timestamp"] = rdf["_timestamp"].apply(
     lambda x: strftime("%Y-%m-%d %H:%M:%S %Z", gmtime(x))
 )
 
 # %% 
-# Columns for what was a training dataset and if it was trained on:
-
-# Check if in runs_df['ds1'] column (json string) has train.slicing != '[:0]':
-def check_if_trained_on(ds_col):
-    def check(row):
-        ds = row[ds_col]
-        if not isinstance(ds, dict):
-            return None
-
-        train_slicing = ds.get("train", {}).get("slicing", None)
-        enable = ds.get("enable", None)
-        if train_slicing is None or enable is None:
-            return None
-        return train_slicing != "[:0]" and enable
-
-    return check
-
+# Set ds1 and ds2 cols (what datasets were used):
 
 def get_ds_name(ds_col):
     def g(row):
@@ -142,49 +126,67 @@ def get_ds_name(ds_col):
 
     return g
 
-
 for i, ds in enumerate(["ds1", "ds2"]):
-    runs_df[ds] = runs_df.apply(get_ds_name(ds), axis=1)
-    runs_df[ds] = runs_df[ds].fillna(
-        runs_df["train_datasets"].apply(
+    rdf[f"{ds}_name"] = rdf.apply(get_ds_name(ds), axis=1)
+    rdf[f"{ds}_name"] = rdf[f"{ds}_name"].fillna(
+        rdf["train_datasets"].apply(
             lambda x: x[i] if isinstance(x, list) and i < len(x) else None
         )
     )
-    runs_df[f"{ds}_training"] = runs_df.apply(check_if_trained_on(ds), axis=1)
+
+# %%
+# Set ds1/ds2_training columns (whether the model was trained on ds1/ds2):
+def check_if_trained_on(ds_col):
+    # Check if in runs_df['ds1'] column (json string) has train.slicing != '[:0]':
+    def check(row):
+        ds = row[ds_col]
+        if not isinstance(ds, dict):
+            return None
+
+        slicing = ds.get("train", {}).get("slicing", None)
+        enable = ds.get("enable", None)
+        if slicing is None or enable is None:
+            return None
+        return slicing != "[:0]" and enable
+
+    return check
+
+
+for i, ds in enumerate(["ds1", "ds2"]):
+    rdf[f"{ds}_training"] = rdf.apply(check_if_trained_on(ds), axis=1)
 
 # %%
 # Sampling method
-runs_df["sampling_method"] = runs_df["sampling_method"].fillna(
-    runs_df["ds2"].apply(
-        lambda x: x["sampling_method"]
-        if isinstance(x, dict) and "sampling_method" in x
-        else (
-            x["name"].split("-")[1]
-            if isinstance(x, dict) and "name" in x and x["name"].startswith("LFB-")
-            else None
-        )
-    )
-)
+
+def get_sampling_method(x):
+    if isinstance(x, dict) and "sampling_method" in x:
+        return x["sampling_method"]
+    elif isinstance(x, dict) and "name" in x and x["name"].startswith("LFB-"):
+        return x["name"].split("-")[1]
+    else:
+        return None
+
+rdf["sampling_method"] = rdf["sampling_method"].fillna(rdf["ds2"].apply(get_sampling_method))
 # Remove 'Sampling.' prefix in 'sampfling_method' column:
-runs_df["sampling_method"] = runs_df["sampling_method"].str.replace("Sampling.", "")
+rdf["sampling_method"] = rdf["sampling_method"].str.replace("Sampling.", "")
 
 # %% 
 # Find previous run ds1_training, ds2_training columns using its 'last_checkpoint_path' column that should match current row 'checkpoint_path' column:
 
 def find_prev_run_id(row):
-    prev_rows = runs_df[runs_df.index > row.name][
-        runs_df["last_checkpoint_path"] == row["checkpoint_path"] 
+    prev_rows = rdf[rdf.index > row.name][
+        rdf["last_checkpoint_path"] == row["checkpoint_path"] 
     ]
     if len(prev_rows) > 0:
         # Return last row id in prev_rows:
         return int(prev_rows.index.min())
     return None
 
-runs_df['prev_run_id'] = runs_df.apply(find_prev_run_id, axis=1)
+rdf['prev_run_id'] = rdf.apply(find_prev_run_id, axis=1)
 
 def get_sequence(row):
     if row['prev_run_id'].is_integer():
-        prev_run = runs_df.loc[int(row['prev_run_id'])]
+        prev_run = rdf.loc[int(row['prev_run_id'])]
         if prev_run.any():
             if (prev_run['ds1_training'] == True and row['ds2_training'] == True):
                 return f"{prev_run['ds1']} then {prev_run['ds2']}"
@@ -192,7 +194,7 @@ def get_sequence(row):
                 return f"{prev_run['ds2']} then {prev_run['ds1']}"
     return "-"
 
-runs_df['seq_train'] = runs_df.apply(get_sequence, axis=1)
+rdf['seq_train'] = rdf.apply(get_sequence, axis=1)
 
 # Get number of parameters for a HuggingFace model using 'model_path' field into 'parameters_num':
 model_size_mln = {
@@ -202,22 +204,22 @@ model_size_mln = {
     'microsoft/deberta-v2-xlarge': 884,
 
 }
-runs_df["model_size_mln"] = runs_df["model_path"].apply(
+rdf["model_size_mln"] = rdf["model_path"].apply(
     lambda x: model_size_mln.get(x, None)
 )
 
 # %%
 # To csv:
-runs_df.to_csv("report/project.csv")
+rdf.to_csv("report/project.csv")
 
 # %%
-runs_df = pd.read_csv("report/project.csv")
+rdf = pd.read_csv("report/project.csv")
 
 # %%
 # Create a view
 
 # Filter out runs with with
-myview = runs_df[
+myview = rdf[
     [
         "model_path",
         "timestamp",
@@ -229,13 +231,13 @@ myview = runs_df[
         "_step",
     ]
 ]
-bs_columns = [col for col in runs_df.columns if col.startswith("bs_")]
-myview["bs_max"] = runs_df[bs_columns].max(axis=1)
-myview["bs_std"] = runs_df[bs_columns].std(axis=1)
+bs_columns = [col for col in rdf.columns if col.startswith("bs_")]
+myview["bs_max"] = rdf[bs_columns].max(axis=1)
+myview["bs_std"] = rdf[bs_columns].std(axis=1)
 #cod_columns = [col for col in runs_df.columns if col.startswith("cod_")]
 #myview["cod_median"] = runs_df[cod_columns].median(axis=1)
 #myview["cod_std"] = runs_df[cod_columns].std(axis=1)
-myview["steps"] = runs_df["_step"]
+myview["steps"] = rdf["_step"]
 myview = myview.sort_values(by=["model_size_mln"])
 myview
 # %%
